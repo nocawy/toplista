a2ee3bd221c9c5be56d8faa7410c0cb96ae71c03
Nocawy <nocawy@users.noreply.github.com>
2024-02-08 05:20:21 +0100

    create initial database model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    s_yt_id = models.CharField(max_length=11, unique=True)  # Youtube ID
+    s_artist = models.CharField(max_length=99)
+    s_title = models.CharField(max_length=99)
+    s_album = models.CharField(max_length=99, blank=True, null=True)
+    s_released = models.IntegerField()
+    s_discovered = models.IntegerField()
+    s_comment = models.TextField(blank=True, null=True)
+    s_last_updated = models.DateTimeField(auto_now=True)
+    s_created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.s_yt_id} - {self.s_title}"
+
+
+class Rank(models.Model):
+    song = models.OneToOneField(Song, on_delete=models.CASCADE, related_name='rank')
+    r_rank = models.IntegerField()
+    r_last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.r_rank} - {self.song.s_yt_id} - {self.song.s_title}"
