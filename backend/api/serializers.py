e320c2a5cc8d1502375274ec866744c99eadb1fe
Nocawy <nocawy@users.noreply.github.com>
2024-02-09 05:05:53 +0100

    update api/songs/ endpoint to include r_rank

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Song
 
 class SongSerializer(serializers.ModelSerializer):
+    r_rank = serializers.IntegerField(source='rank.r_rank', read_only=True)
+    
     class Meta:
         model = Song
-        fields = '__all__'
+        fields = [field.name for field in Song._meta.fields] + ['r_rank']
