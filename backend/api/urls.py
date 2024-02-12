4511ae0df26aa024547d03347937ff96ada2a166
Nocawy <nocawy@users.noreply.github.com>
2024-02-12 11:35:19 +0100

    remove unused code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import hello_world
 from .views import SongList
 
 urlpatterns = [
-    path('hello/', hello_world),
-    path('songs/', SongList.as_view(), name='song-list'),
+    path('songs/', SongList.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,3 @@
-from django.shortcuts import render
-
-
-from django.http import JsonResponse
-
-def hello_world(request):
-    return JsonResponse({'message': 'hello world!!'})
-
-
 from rest_framework import generics
 from .models import Song
 from .serializers import SongSerializer
