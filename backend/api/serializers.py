d6111290b8703d163c776876bc1f82b85d14f742
Nocawy <nocawy@users.noreply.github.com>
2024-02-22 17:48:36 +0100

    validate YT_ID length in SongSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,3 +7,9 @@ class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = [field.name for field in Song._meta.fields] + ['r_rank']
+
+    def validate_s_yt_id(self, value):
+        """Checks if the s_yt_id has exactly 11 characters."""
+        if len(value) != 11:
+            raise serializers.ValidationError("YT ID must be exactly 11 characters long.")
+        return value
