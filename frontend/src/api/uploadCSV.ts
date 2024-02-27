a532349ab2faef723b74910dcb92cc490bb70809
Nocawy <nocawy@users.noreply.github.com>
2024-02-27 21:39:58 +0100

    use apiClient for uploadCSV

--- frontend/src/api/uploadCSV.ts
+++ frontend/src/api/uploadCSV.ts
@@ -1,23 +1,14 @@
+// api/uploadCSV.ts
+import apiClient from "./apiClient";
+
 const uploadCSV = async (file: File): Promise<void> => {
   const formData = new FormData();
   formData.append("file", file);
-
   try {
-    const response = await fetch(
-      `${process.env.REACT_APP_API_URL}upload-csv/`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-
-    if (response.ok) {
-      console.log("File has been successfully uploaded.");
-    } else {
-      console.error("There was an error during the file upload.");
-    }
+    const response = await apiClient.post("upload-csv/", formData, {});
+    console.log("File has been successfully uploaded: ", response.data);
   } catch (error) {
-    console.error("Error:", error);
+    console.error("There was an error during the file upload:", error);
   }
 };
 
